83fa5f4b112d7766e8b9888318dc185a74a8fb41
aarongiesbrecht <aaronr.giesbrecht@gmail.com>
2019-11-19 10:51:38 -0800

    beggining launch screen added

---
+++ slimes/lib/pages/CharacterSelectionPage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CharacterSelectionPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Material(
+      child: Scaffold(
+        body: Column(
+          children: <Widget>[
+            
+          ],
+        ),  
+      ),
+    );
+  }
+}
---
+++ slimes/lib/pages/LandingPage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+//local imports
+import './CharacterSelectionPage.dart';
+
+class LandingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Material(
+      color: Colors.green[300],
+      child: new InkWell(
+        onTap: () => Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new CharacterSelectionPage())),
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text('Start', style: new TextStyle(color: Colors.black, fontSize: 50.0, fontWeight: FontWeight.bold)),
+            new Text('Game', style: new TextStyle(color: Colors.black, fontSize: 50.0, fontWeight: FontWeight.bold)),
+          ],       
+        ),
+      ),     
+    );
+  }
+}
