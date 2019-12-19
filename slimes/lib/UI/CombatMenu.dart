cc690f8b07bafacee23cef8d83a7f3b4635c1ace
aarongiesbrecht <aaronr.giesbrecht@gmail.com>
2019-12-19 10:19:33 -0800

    lifted char temp data to higher state

--- slimes/lib/UI/CombatMenu.dart
+++ slimes/lib/UI/CombatMenu.dart
@@ -11,38 +11,19 @@ class CombatMenu extends StatefulWidget {
   //! TODO vars needed
   //* enemy list
   //* dungeon info (loc, rooms, enemy split)
-  //final List<Slime> chars;
+  final List<Slime> chars;
   
-  CombatMenu();
+  CombatMenu(this.chars);
   @override
   State createState() => CombatMenuState();
 }
 
 class CombatMenuState extends State<CombatMenu> {
 
-//! temp character data !
-  static List<Move> moves = [
-    new Move('_name0', 10),
-    new Move('_name1', 10),
-    new Move('_name2', 10),
-    new Move('_name3', 10),
-    new Move('_name4', 10),
-    new Move('_name5', 10),
-  ];
-  List<Slime> chars = [
-    new Slime('fire', 'anima', 50, moves),
-    new Slime('fire', 'anima', 60, moves),
-    new Slime('fire', 'anima', 30, moves),
-    new Slime('fire', 'anima', 70, moves),
-    new Slime('fire', 'anima', 20, moves),
-    new Slime('fire', 'anima', 50, moves),
-  ];
-//!!
-
   //local ontap method
   void _onTap(int index) {
     setState(() {
-      chars[index].damage(10);
+      widget.chars[index].damage(10);
     });
   }
 
@@ -75,12 +56,12 @@ class CombatMenuState extends State<CombatMenu> {
               crossAxisCount: 2,
               childAspectRatio: 3.1,
               children: <Widget>[
-                new CombatMenuTile('one', () => _onTap(0), true, chars[0]),
-                new CombatMenuTile('two', () => _onTap(1), true, chars[1]),
-                new CombatMenuTile('three', () => _onTap(2), true, chars[2]),
-                new CombatMenuTile('four', () => _onTap(3), true, chars[3]),
-                new CombatMenuTile('five', () => _onTap(4), true, chars[4]),
-                new CombatMenuTile('six', () => _onTap(5), true, chars[5]),
+                new CombatMenuTile('one', () => _onTap(0), true, widget.chars[0]),
+                new CombatMenuTile('two', () => _onTap(1), true, widget.chars[1]),
+                new CombatMenuTile('three', () => _onTap(2), true, widget.chars[2]),
+                new CombatMenuTile('four', () => _onTap(3), true, widget.chars[3]),
+                new CombatMenuTile('five', () => _onTap(4), true, widget.chars[4]),
+                new CombatMenuTile('six', () => _onTap(5), true, widget.chars[5]),
               ],
             ),
           ),
--- slimes/lib/pages/CombatPage.dart
+++ slimes/lib/pages/CombatPage.dart
@@ -9,6 +9,25 @@ class CombatPage extends StatefulWidget {
   State createState() => CombatPageState();
 }
 
+//! temp character data !
+List<Move> moves = [
+  new Move('_name0', 10),
+  new Move('_name1', 10),
+  new Move('_name2', 10),
+  new Move('_name3', 10),
+  new Move('_name4', 10),
+  new Move('_name5', 10),
+];
+List<Slime> chars = [
+  new Slime('fire', 'anima', 50, moves),
+  new Slime('fire', 'anima', 60, moves),
+  new Slime('fire', 'anima', 30, moves),
+  new Slime('fire', 'anima', 70, moves),
+  new Slime('fire', 'anima', 20, moves),
+  new Slime('fire', 'anima', 50, moves),
+];
+//!!
+
 //navbar items
 List itemBar = <BottomNavigationBarItem>[
   BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('home')),
@@ -38,7 +57,7 @@ class CombatPageState extends State<CombatPage> {
             Expanded(
               child: Center(
                 child: Container(
-                  child: CombatMenu(),
+                  child: CombatMenu(chars),
                 ),
               ),
             ),
